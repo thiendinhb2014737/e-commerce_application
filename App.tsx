fb9be9c06eed8db0677db6014dc4ace5184dd518
ThienDinh <thiendinh25062@gmail.com>
2024-02-20 21:35:54 +0700

    add onbroadingScreen

--- App.tsx
+++ App.tsx
@@ -4,22 +4,39 @@ import { SplashScreen } from './src/screens'
 import AuthNavigator from './src/navigators/AuthNavigator'
 import { NavigationContainer } from '@react-navigation/native'
 import { StatusBar } from 'react-native'
+import { useAsyncStorage } from '@react-native-async-storage/async-storage'
+import MainNavigator from './src/navigators/MainNavigator'
 
 const App = () => {
   const [isShownSplash, setIsShownSplash] = useState(true)
+  const [accessToken, setAccessToken] = useState('')
+  const { getItem, setItem } = useAsyncStorage('access_token')
+
   useEffect(() => {
     const timeout = setTimeout(() => {
       setIsShownSplash(false)
     }, 1500)
     return () => clearTimeout(timeout)
   }, [])
+
+  useEffect(() => {
+    checkLogin()
+  }, [])
+
+
+  const checkLogin = async () => {
+    const token = await getItem()
+    token && setAccessToken(token)
+  }
+
   return (
     <>
       <StatusBar barStyle='dark-content' backgroundColor='transparent' translucent />
       {
-        isShownSplash ? (<SplashScreen />) : (<NavigationContainer>
-          <AuthNavigator />
-        </NavigationContainer>
+        isShownSplash ? (<SplashScreen />) : (
+          <NavigationContainer>
+            {accessToken ? <MainNavigator /> : <AuthNavigator />}
+          </NavigationContainer>
         )
       }
     </>
--- src/navigators/AuthNavigator.tsx
+++ src/navigators/AuthNavigator.tsx
@@ -2,6 +2,7 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { LoginScreen } from '../screens'
+import OnBroadingScreen from '../screens/auth/OnBroadingScreen'
 
 const AuthNavigator = () => {
     const Stack = createNativeStackNavigator()
@@ -9,6 +10,7 @@ const AuthNavigator = () => {
         <Stack.Navigator screenOptions={{
             headerShown: false
         }}>
+            <Stack.Screen name='OnBroadingScreen' component={OnBroadingScreen} />
             <Stack.Screen name='LoginScreen' component={LoginScreen} />
         </Stack.Navigator>
     )
